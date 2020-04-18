8e225b4343c900f66c901f3bd500e185b9d83606
Chantelsky <chantel.sky@outlook.com>
2020-04-19 02:10:19 +1000

    login auth

--- app/http/web/my-app/src/Login/index.js
+++ app/http/web/my-app/src/Login/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+import { Redirect } from 'react-router-dom';
+import { withAuth } from '@okta/okta-react';
 
-const index = () => {
-  return <div></div>;
+const Login = (props) => {
+  const [auth, setAuth] = useState(null);
+
+  const checkAuthentication = async () => {
+    const authenticated = await props.auth.isAuthenticated();
+    if (authenticated !== auth) {
+      setAuth(authenticated);
+    }
+  };
+
+  useEffect(() => {
+    checkAuthentication();
+  }, []);
+
+  const login = async (e) => {
+    props.auth.login('/home');
+  };
+
+  return auth ? (
+    <Redirect to='/home' />
+  ) : (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Button variant='contained' color='primary' onClick={login}>
+        Login with Okta
+      </Button>
+    </div>
+  );
 };
 
-export default index;
+export default withAuth(Login);
